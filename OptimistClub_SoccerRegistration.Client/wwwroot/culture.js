287cffc1db13ed4106ad43abc9d052f724117261
wolfisa <isabelle.laporte@hotmail.com>
2026-02-02 20:04:49 -0500

    Front End and localization fixed

---
+++ OptimistClub_SoccerRegistration.Client/wwwroot/culture.js
@@ -0,0 +1,16 @@
+﻿window.blazorCulture = {
+    get: function () {
+        const name = '.AspNetCore.Culture';
+        const match = document.cookie
+            .split('; ')
+            .find(row => row.startsWith(name + '='));
+
+        if (!match) return null;
+
+        const value = decodeURIComponent(match.split('=')[1]);
+        // value looks like: c=fr-CA|uic=fr-CA
+
+        const cultureMatch = value.match(/c=([^|]+)/);
+        return cultureMatch ? cultureMatch[1] : null;
+    }
+};
---
+++ OptimistClub_SoccerRegistration/Components/CultureController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OptimistClub_SoccerRegistration.Components
+{
+    [Route("Culture")]
+    public class CultureController : Controller
+    {
+        [HttpGet("Set")]
+        public IActionResult Set(string culture, string redirectUri)
+        {
+            if (!string.IsNullOrEmpty(culture))
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(
+                        new RequestCulture(culture)
+                    ),
+                    new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddYears(1)
+                    }
+                );
+            }
+
+            return string.IsNullOrWhiteSpace(redirectUri)
+                ? Redirect("/")
+                :LocalRedirect(redirectUri);
+            }
+    }
+}
